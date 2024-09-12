39167ccf0d30eb1283c762c6238da3664655de29
Muhammad Amit Hasan <bhamithasan@gmail.com>
2024-09-12 21:21:35 +0600

    Update test project

--- test/src/App.js
+++ test/src/App.js
@@ -6,6 +6,8 @@ const App = () => {
 
   return (
     <div className='app'>
+      <h1>Test Project to check the</h1>
+      <h4>react-login-form-package</h4>
       <LoginComponent />
     </div>
   );
